a9093542d8c373f2f6fecf1b06bee04ee15f06de
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2018-09-02 09:30:19 +0800

    snap: Set-up version script
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

---
+++ snap/utilities/set-snap-version.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Program to set snap's version, used by the `version-script` keyword
+# 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com> © 2018
+
+set \
+	-o errexit \
+	-o errtrace \
+	-o nounset \
+	-o pipefail
+
+init(){
+	local \
+		upstream_version \
+		packaging_revision
+
+	upstream_version=u16.04-"$(date +%Y%m%d)"
+
+	packaging_revision="$(
+		git \
+			describe \
+			--abbrev=4 \
+			--always \
+			--match nothing \
+			--dirty=-d
+	)"
+
+	printf \
+		-- \
+		'%s' \
+		"${upstream_version}+pkg-${packaging_revision}"
+
+	exit 0
+}
+
+init "${@}"
